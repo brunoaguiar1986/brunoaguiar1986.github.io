1a80e211496ac966e2761ba630078876b777017f
brunoaguiar1986 <85034205+brunoaguiar1986@users.noreply.github.com>
2022-07-31 21:25:37 -0300

    mario

---
+++ js/script.js
@@ -0,0 +1,45 @@
+const mario = document.querySelector('.mario');
+        const pipe = document.querySelector('.pipe');
+        var sound1 = new Audio('music/super_mario_theme_song.mp3');
+        var sound2 = new Audio('music/mario-game-over.mp3');
+
+        function myFunction() {
+                sound1.play();
+            }
+
+        const jump = () => {
+            mario.classList.add('jump');
+
+            setTimeout(() => {
+
+                mario.classList.remove('jump');
+
+            }, 500);
+        }
+
+        const loop = setInterval(() => {
+
+            const pipePosition = pipe.offsetLeft;
+            const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+            if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+
+                pipe.style.animation = 'none';
+                pipe.style.left = `${pipePosition}px`;
+
+                mario.style.animation = 'none';
+                mario.style.bottom = `${marioPosition}px`;
+
+                mario.src = 'images/game-over.png';
+                mario.style.width = '75px';
+                mario.style.marginLeft = '50px';
+                sound1.pause();
+                sound2.play();
+
+
+                clearInterval(loop);
+            }
+
+        }, 10);
+
+        document.addEventListener('keydown', jump);
